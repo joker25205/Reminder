77130dfdb42d31a57cfe4085c6e212e2e817829f
Vasyl Scherban <joker25205@gmail.com>
2016-01-30 01:38:35 +0200

    He added SearchView, implemented a search in SQLite database

--- app/src/main/java/com/example/joker/reminder/adapter/CurrentTasksAdapter.java
+++ app/src/main/java/com/example/joker/reminder/adapter/CurrentTasksAdapter.java
@@ -70,8 +70,8 @@ public class CurrentTasksAdapter extends TaskAdapter {
             }
 
             itemView.setVisibility(View.VISIBLE);
+            taskViewHolder.priority.setEnabled(true);
 
-            itemView.setBackgroundColor(resources.getColor(R.color.gray_50));
 
             taskViewHolder.title.setTextColor(resources.getColor(R.color.primary_text_default_material_light));
             taskViewHolder.date.setTextColor(resources.getColor(R.color.secondary_text_default_material_light));
@@ -97,11 +97,12 @@ public class CurrentTasksAdapter extends TaskAdapter {
                 @Override
                 public void onClick(View v) {
 
+                    taskViewHolder.priority.setEnabled(false);
+
                     task.setStatus(ModelTask.STATUS_DONE);
 
                     getTaskFragment().activity.dbHelper.update().status(task.getTimeStamp(), ModelTask.STATUS_DONE);
 
-                    itemView.setBackgroundColor(resources.getColor(R.color.gray_200));
 
                     taskViewHolder.title.setTextColor(resources.getColor(R.color.primary_text_disabled_material_light));
                     taskViewHolder.date.setTextColor(resources.getColor(R.color.secondary_text_disabled_material_light));
--- app/src/main/java/com/example/joker/reminder/adapter/DoneTasksAdapter.java
+++ app/src/main/java/com/example/joker/reminder/adapter/DoneTasksAdapter.java
@@ -62,7 +62,9 @@ public class DoneTasksAdapter extends TaskAdapter {
 
             itemView.setVisibility(View.VISIBLE);
 
-            itemView.setBackgroundColor(resources.getColor(R.color.gray_200));
+            taskViewHolder.priority.setEnabled(true);
+
+
 
             taskViewHolder.title.setTextColor(resources.getColor(R.color.primary_text_disabled_material_light));
             taskViewHolder.date.setTextColor(resources.getColor(R.color.secondary_text_disabled_material_light));
@@ -87,12 +89,14 @@ public class DoneTasksAdapter extends TaskAdapter {
             taskViewHolder.priority.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+
+                    taskViewHolder.priority.setEnabled(false);
+
                     task.setStatus(ModelTask.STATUS_CURRENT);
 
                     getTaskFragment().activity.dbHelper.update().status(task.getTimeStamp(), ModelTask.STATUS_CURRENT);
 
 
-                    itemView.setBackgroundColor(resources.getColor(R.color.gray_50));
 
                     taskViewHolder.title.setTextColor(resources.getColor(R.color.primary_text_default_material_light));
                     taskViewHolder.date.setTextColor(resources.getColor(R.color.secondary_text_default_material_light));
